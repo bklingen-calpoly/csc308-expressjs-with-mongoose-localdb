016d529b969018720cf6c6e7aecbdb2f45fa5a4c
BJ Klingenberg <75038922+bklingen-calpoly@users.noreply.github.com>
2024-01-31 15:34:11 -0800

    Update backend_with_db.js
    update port to 8000

--- backend_with_db.js
+++ backend_with_db.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const userServices = require('./models/user-services');
 
 const app = express();
-const port = 5000;
+const port = 8000;
 
 app.use(cors());
 app.use(express.json());
